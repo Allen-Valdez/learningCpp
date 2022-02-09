a4dea307ea5b6c128aa59ddaeeab38c3d96791f4
Allen Valdez <AllenValdez1728@gmail.com>
2022-02-08 16:32:09 -0800

    My first C++ project

---
+++ Projects/BlockLetters/blockLetters.cpp
@@ -0,0 +1,12 @@
+// Fun Fact: I served in the Marines for 4 years as an Infantry SquadLeader
+
+#include <iostream>
+
+int main()
+{
+  std::cout << "   A          V               V\n";
+  std::cout << "A      A        V           V\n";
+  std::cout << "AAAAAAAA          V       V\n";
+  std::cout << "A      A            V   V\n";
+  std::cout << "A      A              V\n";
+};
