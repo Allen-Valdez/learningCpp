0407bf7d5b1aca4ed14f003797f54e158d8ddca8
Allen Valdez <AllenValdez1728@gmail.com>
2022-02-24 16:18:48 -0800

    Completed a project using vectors, c++ string and range based for loops

---
+++ Projects/WhaleTalk/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+int main()
+{
+  string phrase{"a whale of a deal!"};
+
+  vector<char> vowels{'a', 'e', 'i', 'o', 'u'};
+  vector<char> result{};
+
+  for (auto c : phrase)
+  {
+    for (auto ch : vowels)
+    {
+      if (ch == c && c == 'e' || ch == c && c == 'u')
+      {
+        result.push_back(c);
+        result.push_back(c);
+      }
+      else if (ch == c)
+      {
+        result.push_back(c);
+      }
+    }
+  }
+
+  // Time to post the result
+  for (auto c : result)
+  {
+    cout << c;
+  }
+  cout << endl;
+}
